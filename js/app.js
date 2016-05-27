1002cc19c9d0c6d4cb78501d634a181fd407c6e4
Joseph Tongay <dirk.tongay@gmail.com>
2016-05-26 22:01:02 -0500

    test keypress

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
       $('.ryu-throwing').hide();
       $('.ryu-ready').show();
     })
-  $('html').keydown(function (e) {
+  $('html').keypress(function (e) {
       if (e.which === 88) {
         playRyu();
         $('.ryu-cool').show();
