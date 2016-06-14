7a4f90de138ada60d691926f67fcee8ccbe48452
Joseph Tongay <dirk.tongay@gmail.com>
2016-06-14 17:56:50 -0500

    bug

--- js/app.js
+++ js/app.js
@@ -1,46 +1,58 @@
-$(document).ready(function () {
-  $('.ryu').mouseenter(function () {
-      $('.ryu-still').hide();
-      $('.ryu-ready').show();
-    })
-    .mouseleave(function () {
-      $('.ryu-ready').hide();
-      $('.ryu-still').show();
-    })
-    .mousedown(function () {
-      playHadouken();
-      $('.ryu-ready').hide();
-      $('.ryu-throwing').show();
-      $('.hadouken').finish().show().animate({ 'left': '1020px' },
-        500,
-        function () {
-          $(this).hide();
-          $(this).css('left', '520px');
-        });
-    })
-    .mouseup(function () {
-      $('.ryu-throwing').hide();
-      $('.ryu-ready').show();
-    })
-  $('html').keydown(function (e) {
-      if (e.which === 88) {
-        $('.ryu-cool').show();
+(function () {
+
+  $(document).ready(function () {
+    $('.ryu').mouseenter(function () {
         $('.ryu-still').hide();
+        $('.ryu-ready').show();
+      })
+      .mouseleave(function () {
         $('.ryu-ready').hide();
-      }
-    })
-    .keyup(function () {
-      $('.ryu-cool').hide();
-      $('.ryu-still').show();
-      $('.ryu-ready').hide();
-    });
+        $('.ryu-still').show();
+      })
+      .mousedown(function () {
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate({ 'left': '1020px' },
+          500,
+          function () {
+            $(this).hide();
+            $(this).css('left', '520px');
+          });
+      })
+      .mouseup(function () {
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+      })
+    $('html').keydown(function (e) {
+        if (e.which === 88) {
+          playRyu();
+          $('.ryu-cool').show();
+          $('.ryu-still').hide();
+          $('.ryu-ready').hide();
+        }
+      })
+      .keyup(function () {
+        $('.ryu-cool').hide();
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+
+      });
 
 
 
-});
+  });
 
-function playHadouken() {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
+  function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+  }
+
+  function playRyu() {
+    $('#ryu-music')[0].volume = 0.8;
+    $('#ryu-music')[0].load();
+    $('#ryu-music')[0].play();
+
+  }
+})();
